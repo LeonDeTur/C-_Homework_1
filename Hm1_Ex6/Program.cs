3c126c17890455cd59fca329bc16829fb091dc4e
Leon <turkov.leon-2@yandex.ru>
2022-08-24 01:22:33 +0300

    Exercise 6

---
+++ Hm1_Ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ервое число");
+int num_a = Convert.ToInt32(Console.ReadLine());
+
+if (num_a % 2 == 0)
+{
+    Console.WriteLine("Число чётное"); 
+}
+else
+{
+    Console.WriteLine("Число нечётное"); 
+}
