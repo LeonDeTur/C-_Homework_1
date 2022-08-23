9c40b41f583fd63b66f8790004e90be74b57a1a6
Leon <turkov.leon-2@yandex.ru>
2022-08-24 01:22:55 +0300

    Exercise 8

---
+++ Hm1_Ex8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num_a = Convert.ToInt32(Console.ReadLine());
+int num_car = 0;
+
+Console.WriteLine("Чётные числа от " + num_car + " до " + num_a + ":");
+
+while (num_car < num_a - 1)
+{
+    num_car += 2;
+    Console.WriteLine(num_car);
+}
